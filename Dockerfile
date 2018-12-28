716effc0e0a34fb111ad3fb86c936b8d2de72666
Rafael Turk <rafael.turk@gmail.com>
2018-12-27 22:20:45 -0200

    simpler, faster, version. Compatible with MongoDb 4.x

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,18 @@
-FROM debian:jessie-slim
-
-ENV MONGO_MAJOR 3.4
-ENV MONGO_VERSION 3.4.4
-
-# MongoDB Enviroment
-# MongoDB Source Database
-ENV MONGO_DBNAME=my-db
-ENV MONGO_HOST=localhost
-
-# S3 bucket that the backup will be stored
-ENV AWS_TARGET_BUCKET=s3-bucket-name
-ENV AWS_ACCESS_KEY_ID=AAAAAAA
-ENV AWS_SECRET_ACCESS_KEY=BBBBBB
-
-COPY backup-mongodb-to-s3.sh /scripts/
-
-# Setup links to mongoDB client
-RUN echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/$MONGO_MAJOR main" > /etc/apt/sources.list.d/mongodb-org.list
-RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+FROM mongo:4.0.4-xenial
 
 # Preparate update
 RUN apt-get update
-
-# Install Mongodump from MongoDB-org-tools pacakge
-RUN apt-get install -y mongodb-org-tools=$MONGO_VERSION
-
-# Install the AWS Cli
 RUN apt-get install -y awscli
 
-#Cleanup
-RUN rm -rf /var/lib/apt/lists/*
+COPY backup-mongodb-to-s3.sh /scripts/
+
+# MongoDB Source Database
+ENV MONGO_URI="mongodb://localhost/test"
+
+# S3 bucket that the backup will be stored
+ENV AWS_TARGET_BUCKET="s3://mybucket/folder/"
+ENV AWS_ACCESS_KEY_ID=AAAAAAA
+ENV AWS_SECRET_ACCESS_KEY=BBBBBB
 
 #Execute Backup
 CMD ["/bin/sh", "/scripts/backup-mongodb-to-s3.sh"]
--- backup-mongodb-to-s3.sh
+++ backup-mongodb-to-s3.sh
@@ -1,18 +1,6 @@
 #From https://gist.github.com/eladnava/96bd9771cd2e01fb4427230563991c8d
 #!/bin/sh
 
-# Make sure to:
-# 1) Name this file `backup.sh` and place it in /home/ubuntu
-# 2) Run sudo apt-get install awscli to install the AWSCLI
-# 3) Run aws configure (enter s3-authorized IAM user and specify region)
-# 4) Fill in DB host + name
-# 5) Create S3 bucket for the backups and fill it in below (set a lifecycle rule to expire files older than X days in the bucket)
-# 6) Run chmod +x backup.sh
-# 7) Test it out via ./backup.sh
-# 8) Set up a daily backup at midnight via `crontab -e`:
-#    0 0 * * * /home/ubuntu/backup.sh > /home/ubuntu/backup.log
-
-
 # Current time
 TIME=`/bin/date +%Y-%m-%d-%Hh%Ms%S`
 
@@ -25,18 +13,17 @@ DEST=/volume/tmp
 # Tar file of backup directory
 TAR=$DEST/../$TIME.tar
 
-
 # Log
-echo "Backing up $MONGO_HOST/$MONGO_DBNAME to s3://$AWS_TARGET_BUCKET/ on $TIME";
+echo "Backing up $MONGO_URI to $AWS_TARGET_BUCKET on $TIME";
 
 # Dump from mongodb host into backup directory
-/usr/bin/mongodump -h $MONGO_HOST -d $MONGO_DBNAME -o $DEST
+/usr/bin/mongodump --uri $MONGO_URI -o $DEST
 
 # Create tar of backup directory
 /bin/tar cvf $TAR -C $DEST .
 
 # Upload tar to s3
-/usr/bin/aws s3 cp $TAR s3://$AWS_TARGET_BUCKET/
+/usr/bin/aws s3 cp $TAR $AWS_TARGET_BUCKET
 
 # Remove tar file locally
 /bin/rm -f $TAR
@@ -45,4 +32,4 @@ echo "Backing up $MONGO_HOST/$MONGO_DBNAME to s3://$AWS_TARGET_BUCKET/ on $TIME"
 /bin/rm -rf $DEST
 
 # All done
-echo "Backup available at https://s3.amazonaws.com/$AWS_TARGET_BUCKET/$TIME.tar"
+echo "Backup available at $AWS_TARGET_BUCKET/$TIME.tar"

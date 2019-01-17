e383e8bab7899a2a9e16130551534e6ea46154ba
Rafael Turk <rafael.turk@gmail.com>
2019-01-16 23:10:47 -0200

    improve script, gzip using mongodump

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 MongoDB Backup Docker
 
 This Image is intended to be used in a Kubernetes environment to automatically backup MonoDB to a S3 bucket.
+
+# upload
+
+docker login
+docker build . -t entria/mongodb-backup-docker
+
+# Powered by
+[Entria](https://entria.com.br)
+[Feedback](https://feedback.house)
+
+# Inspired by
+https://gist.github.com/eladnava/96bd9771cd2e01fb4427230563991c8d
--- backup-mongodb-to-s3.sh
+++ backup-mongodb-to-s3.sh
@@ -1,37 +1,28 @@
-#From https://gist.github.com/eladnava/96bd9771cd2e01fb4427230563991c8d
 #!/bin/sh
 
 # Current time, this will be the file name
-FILENAME=`/bin/date +%Y-%m-%d-%Hh%Ms%S`
+FILENAME=`/bin/date +%Y-%m-%d-%Hh%Ms%S`.gz
 # Separate Uploads using date as folders
 TIMEPATH=`/bin/date +"%Y/%m/%d"` 
 
-# Backup directory
+# temp directory
 DEST=/volume/tmp
-
 # Create backup dir (-p to avoid warning if already exists)
 /bin/mkdir -p $DEST
 
-# Tar file of backup directory
-TAR=$DEST/../$FILENAME.tar
+FILENAME_FULL=$DEST/$FILENAME
 
 # Log
 echo "Backing up $MONGO_URI to $AWS_TARGET_BUCKET/$TIMEPATH on $FILENAME";
 
 # Dump from mongodb host into backup directory
-/usr/bin/mongodump --uri $MONGO_URI -o $DEST
-
-# Create tar of backup directory
-/bin/tar cvf $TAR -C $DEST .
+/usr/bin/mongodump --uri $MONGO_URI --archive=$FILENAME_FULL --gzip
 
 # Upload tar to s3
-/usr/bin/aws s3 cp $TAR $AWS_TARGET_BUCKET/$TIMEPATH/$FILENAME.tar
+/usr/bin/aws s3 cp $FILENAME_FULL $AWS_TARGET_BUCKET/$TIMEPATH/$FILENAME
 
 # Remove tar file locally
-/bin/rm -f $TAR
-
-## Remove backup directory
-/bin/rm -rf $DEST
+/bin/rm -f $FILENAME_FULL
 
 # All done
-echo "Backup available at $AWS_TARGET_BUCKET/$TIMEPATH/$FILENAME.tar"
+echo "Backup available at $AWS_TARGET_BUCKET/$TIMEPATH/$FILENAME"
